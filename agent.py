c9f2f5d586f8d07f925351938927852dc0d859b3
galdashen <galdashen@qq.com>
2026-06-14 18:27:24 +0800

    update

--- agent.py
+++ agent.py
@@ -2,7 +2,7 @@ import os,re,subprocess;from openai import OpenAI
 c=OpenAI(api_key=os.getenv("DEEPSEEK_API_KEY"),base_url="https://api.deepseek.com")
 m=[{"role":"system","content":"你可以用```pwsh-action\n命令\n```在用户的电脑上执行pwsh命令，用户会把结果返回给你"}]
 while 1:
- if(u:=input("User: ")).lower() in ["exit","quit"]:break
+ if (u:=input("User: ")).lower() in ["exit","quit"]:break
  m.append({"role":"user","content":u})
  while 1:
   r=c.chat.completions.create(model="deepseek-v4-pro",messages=m).choices[0].message.content;print("Agent:",r);m.append({"role":"assistant","content":r})
